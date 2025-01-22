848961615e42bf85f9d04c09f6e96985e56f5baf
David Figueras <david.f.sosa33@gmail.com>
2025-01-21 20:41:46 -0500

    testing segmentation and depth camera, and new saving method

--- camera_test.py
+++ camera_test.py
@@ -28,20 +28,45 @@ client.takeoffAsync(vehicle_name=drone).join()
 client.moveToPositionAsync(-10, 10, -25, 5).join()
 
 
-responses = client.simGetImages([airsim.ImageRequest("0", airsim.ImageType.DepthVis, False, False)])
-response = responses[0]
+# responses = client.simGetImages([airsim.ImageRequest("0", airsim.ImageType.DepthVis, False, False)])
+responses = client.simGetImages([
+    airsim.ImageRequest(camera_name = "front-0", image_type=airsim.ImageType.DepthVis, pixels_as_float = False, compress = False),  #depth visualization image
+    airsim.ImageRequest("front-0", airsim.ImageType.DepthPerspective, pixels_as_float = True, compress = True), #depth in perspective projection
+    airsim.ImageRequest("front-0", airsim.ImageType.Scene, pixels_as_float = False, compress = True), #scene vision image in png format
+    airsim.ImageRequest("front-0", airsim.ImageType.Scene, pixels_as_float = False, compress = False)])  #scene vision image in uncompressed RGBA array
 
-# get numpy array
-img1d = np.frombuffer(response.image_data_uint8, dtype=np.uint8) 
 
-# reshape array to 4 channel image array H X W X 4
-img_rgb = img1d.reshape(response.height, response.width, 3)
 
-# original image is fliped vertically
-# img_rgb = np.flipud(img_rgb)
 
-# write to png 
-airsim.write_png(os.path.normpath("images/test" + '.png'), img_rgb) 
+for idx, response in enumerate(responses):
+
+    filename = os.path.join("images", str(idx))
+    print("Reponse number: " + str(idx))
+
+    if response.pixels_as_float:
+        print("Type %d, size %d" % (response.image_type, len(response.image_data_float)))
+        airsim.write_pfm(os.path.normpath(filename + '.pfm'), airsim.get_pfm_array(response))
+    elif response.compress: #png format
+        print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
+        airsim.write_file(os.path.normpath(filename + '.png'), response.image_data_uint8)
+    else: #uncompressed array
+        print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
+        img1d = np.frombuffer(response.image_data_uint8, dtype=np.uint8) # get numpy array
+        img_rgb = img1d.reshape(response.height, response.width, 3) # reshape array to 4 channel image array H X W X 3
+        cv2.imwrite(os.path.normpath(filename + '.png'), img_rgb) # write to png
+
+
+# # get numpy array
+# img1d = np.frombuffer(response.image_data_uint8, dtype=np.uint8) 
+
+# # reshape array to 4 channel image array H X W X 4
+# img_rgb = img1d.reshape(response.height, response.width, 3)
+
+# # original image is fliped vertically
+# # img_rgb = np.flipud(img_rgb)
+
+# # write to png 
+# airsim.write_png(os.path.normpath("images/test" + '.png'), img_rgb) 
 
 # end connection
 client.enableApiControl(False)
