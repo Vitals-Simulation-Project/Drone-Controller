23d987eba12e75437ffd2c516f3812eccb514fbf
David Figueras <david.f.sosa33@gmail.com>
2025-01-25 22:19:33 -0500

    gps and relative movement testing

--- movement_function_test.py
+++ movement_function_test.py
@@ -15,8 +15,23 @@ def move_drone(drone_name, x, y, z, speed):
     return client.moveToPositionAsync(new_position.x_val, new_position.y_val, new_position.z_val, speed, vehicle_name=drone_name)
 
 
+
+# prints the state of the drone
+def print_state(drone_name):
+    print("===============================================================")
+    state = client.getMultirotorState(vehicle_name=drone_name)
+    print("state: %s" % pprint.pformat(state))
+    return state
+
+
+# gets GPS coordinates of the drone
+def get_GPS_coordinates(drone_name):
+    state = client.getMultirotorState(vehicle_name=drone_name)
+    return state.gps_location
+
+
 drone = "0"
-drone2 = "1"
+# drone2 = "1"
 
 # directory to store pictures
 imgDir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images')
@@ -33,30 +48,36 @@ client.confirmConnection()
 client.enableApiControl(True, vehicle_name=drone)
 client.armDisarm(True, vehicle_name=drone)
 
-client.enableApiControl(True, vehicle_name=drone2)
-client.armDisarm(True, vehicle_name=drone2)
+
+f1 = client.takeoffAsync(vehicle_name=drone).join()
 
 
-f1 = client.takeoffAsync(vehicle_name=drone)
-f2 = client.takeoffAsync(vehicle_name=drone2)
-f1.join()
-f2.join()
+
 
 # move drone up 25 meters
-f1 = move_drone(drone, 0, 0, -25, 5)
-f2 = move_drone(drone2, 0, 0, -25, 5)
+f1 = move_drone(drone, 0, 0, -25, 5).join()
 
-f1.join()
-f2.join()
+# get initial coordinates
+initial_coords = get_GPS_coordinates(drone)
+print("Initial coordinates: " + str(initial_coords))
 
-f1 = move_drone(drone, 0, 25, 0, 5)
-f2 = move_drone(drone2, 0, 25, 0, 5)
+f1 = move_drone(drone, 0, -20, 0, 5).join()
 
-f1.join()
-f2.join()
+print("Moving back to initial position")
+client.moveToGPSAsync(initial_coords.latitude, initial_coords.longitude, initial_coords.altitude, 5, vehicle_name=drone).join()
+
+# get final coordinates
+final_coords = get_GPS_coordinates(drone)
+print("Final coordinates: " + str(final_coords))
+
+
+# calcualte the distance moved
+distance_moved = np.sqrt((final_coords.latitude - initial_coords.latitude)**2 + (final_coords.longitude - initial_coords.longitude)**2)
+
+
+print("Distance moved: " + str(distance_moved))
 
 
 
 # end connection
 client.enableApiControl(False, vehicle_name=drone)
-client.enableApiControl(False, vehicle_name=drone2)
