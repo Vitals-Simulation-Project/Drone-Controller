e68e6cdc50d5e48a4b5aa1474898ed666941b732
David Figueras <david.f.sosa33@gmail.com>
2025-01-27 11:13:36 -0500

    testing

--- movement_function_test.py
+++ movement_function_test.py
@@ -6,14 +6,45 @@ import pprint
 import time
 
 
+# ===================================== SETUP =====================================
 
-# a function that moves the drone relative to its current position
-def move_drone(drone_name, x, y, z, speed):
+# set up client object to access multirotor drones
+client = airsim.MultirotorClient()
+
+# connect to AirSim simulator
+client.confirmConnection()
+
+# directory to store pictures
+imgDir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images')
+
+# create directory if it does not exist
+if not os.path.exists(imgDir):
+    os.makedirs(imgDir)
+
+# =================================================================================
+
+
+
+
+
+
+# moves the drone relative to its current position
+def move_drone_relative(drone_name, x, y, z, speed):
     state = client.getMultirotorState(vehicle_name=drone_name)
     current_position = state.kinematics_estimated.position
     #print("Current position: " + str(current_position))
     new_position = airsim.Vector3r(current_position.x_val + x, current_position.y_val + y, current_position.z_val + z)    
     return client.moveToPositionAsync(new_position.x_val, new_position.y_val, new_position.z_val, speed, vehicle_name=drone_name)
+
+
+
+# moves the drone to a specific GPS coordinate
+def move_drone_GPS(drone_name, lat, lon, alt, speed):
+    print("Original position: " + str(get_GPS_coordinates(drone_name)))
+    print("Moving to GPS coordinates: " + str(lat) + ", " + str(lon) + ", " + str(alt))
+    return client.moveToGPSAsync(lat, lon, alt, speed, vehicle_name=drone_name)
+
+
 
 
 
@@ -23,9 +54,6 @@ def print_state(drone_name):
     state = client.getMultirotorState(vehicle_name=drone_name)
     print("state: %s" % pprint.pformat(state))
     return state
-
-
-
 
 
 
@@ -43,20 +71,11 @@ def print_GPS_coordinates(drone_name):
     print("GPS coordinates: " + str(coords))
 
 
+
 drone = "0"
 
-# directory to store pictures
-imgDir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images')
 
-# create directory if it does not exist
-if not os.path.exists(imgDir):
-    os.makedirs(imgDir)
 
-# set up client object to access multirotor drone
-client = airsim.MultirotorClient()
-
-# connect to AirSim simulator
-client.confirmConnection()
 client.enableApiControl(True, vehicle_name=drone)
 client.armDisarm(True, vehicle_name=drone)
 
@@ -64,41 +83,27 @@ client.armDisarm(True, vehicle_name=drone)
 client.takeoffAsync(vehicle_name=drone).join()
 
 
-print(client.getGpsData(vehicle_name=drone))
-
-
 # move drone up 25 meters
-f1 = move_drone(drone, 0, 0, -25, 5).join()
-print("First move done")
-print_state(drone)
+move_drone_relative(drone, 0, 0, -25, 5).join()
 
 # get initial coordinates
-# initial_coords = get_GPS_coordinates(drone)
-# print("Initial coordinates: " + str(initial_coords))
+initial_coords = get_GPS_coordinates(drone)
+print("Initial coordinates: " + str(initial_coords))
 
-f2 = move_drone(drone, 0, -20, 0, 5).join()
-print("Second move done")
-print_state(drone)
-
-# print("Moving back to initial position")
-# client.moveToGPSAsync(initial_coords.latitude, initial_coords.longitude, initial_coords.altitude, 5, vehicle_name=drone).join()
-f3 = move_drone(drone, 0, 20, 0, 5).join()
-print("Third move done")
-print_state(drone)
-
-client.moveToPositionAsync(0, 0, 0, 5, vehicle_name=drone).join()
+# move drone left 20 meters
+f2 = move_drone_relative(drone, 0, -20, 0, 5).join()
 
 
 # get final coordinates
-# final_coords = get_GPS_coordinates(drone)
-# print("Final coordinates: " + str(final_coords))
+final_coords = get_GPS_coordinates(drone)
+print("Final coordinates: " + str(final_coords))
 
 
 # calcualte the distance moved
-#distance_moved = np.sqrt((final_coords.latitude - initial_coords.latitude)**2 + (final_coords.longitude - initial_coords.longitude)**2)
+distance_moved = np.sqrt((final_coords.latitude - initial_coords.latitude)**2 + (final_coords.longitude - initial_coords.longitude)**2)
 
 
-#print("Distance moved: " + str(distance_moved))
+print("Distance moved: " + str(distance_moved))
 
 
 
